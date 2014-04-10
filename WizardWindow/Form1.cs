8f304c8255161f62814143b0d7c3fd20ed3124b0
Simon Colmer <simon_colmer@hotmail.com>
2014-04-10 16:48:01 +0100

    Initial Commit

---
+++ WizardWindow/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WizardWindow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        WebCam webcam;
+        private void mainWinForm_Load(object sender, EventArgs e)
+        {
+            webcam = new WebCam();
+            webcam.InitializeWebCam(ref imgVideo);
+        }
+
+        private void bntStart_Click(object sender, EventArgs e)
+        {
+            webcam.Start();
+        }
+
+        private void bntStop_Click(object sender, EventArgs e)
+        {
+            webcam.Stop();
+        }
+
+        private void bntContinue_Click(object sender, EventArgs e)
+        {
+            webcam.Continue();
+        }
+
+        private void bntCapture_Click(object sender, EventArgs e)
+        {
+            imgCapture.Image = imgVideo.Image;
+        }
+
+        private void bntSave_Click(object sender, EventArgs e)
+        {
+            Helper.SaveImageCapture(imgCapture.Image);
+        }
+
+        private void bntVideoFormat_Click(object sender, EventArgs e)
+        {
+            webcam.ResolutionSetting();
+        }
+
+        private void bntVideoSource_Click(object sender, EventArgs e)
+        {
+            webcam.AdvanceSetting();
+        }
+
+        
+    }
+}
